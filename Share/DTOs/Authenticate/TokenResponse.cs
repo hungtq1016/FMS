2248225534662aac1f52adefaea99bff341524cb
hungtq1016 <hungtq1016@gmail.com>
2024-01-15 17:48:20 +0700

    #14 Add authenticate DTO

---
+++ Share/DTOs/Authenticate/TokenResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Share.DTOs
+{
+    public class TokenResponse
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
