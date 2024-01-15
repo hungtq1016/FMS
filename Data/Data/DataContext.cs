331e9a1ef3107356bdad9bef17ccd7fb315c612c
hungtq1016 <hungtq1016@gmail.com>
2024-01-15 17:13:02 +0700

    #8 Add datacontext

---
+++ Data/Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Share.Entities;
+
+namespace Data.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            
+        }
+        public DataContext(DbContextOptions options): base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Status> Statuses => Set<Status>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = $"Data Source=DESKTOP-OCBT336\\SQLEXPRESS;Initial Catalog=FlightManagementSystem;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
