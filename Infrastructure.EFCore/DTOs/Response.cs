218f4a6c9466e42a766c8fbd92efab262f9bc090
hungtq1016 <hungtq1016@gmail.com>
2024-01-26 11:43:08 +0700

    update: format name folder

---
+++ Infrastructure.EFCore/DTOs/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.EFCore.DTOs
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+        public bool IsError { get; set; }
+    }
+}
