a2e54fe2b6e44c749b837e1d496158667384fca3
hungtq1016 <hungtq1016@gmail.com>
2024-01-26 11:40:30 +0700

    enhancement: create new abstract DbContext

---
+++ Infrastructure.EFCore/Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
+
+namespace Infrastructure.EFCore.Data
+{
+    public class AppDbContext : DbContext, IDomainEventContext, IDbFacadeResolver
+    {
+        public AppDbContext(DbContextOptions options) : base(options){}
+
+        protected AppDbContext(){}
+
+        public IEnumerable<IDomainEvent> GetDomainEvents()
+        {
+            var domainEntities = ChangeTracker
+                .Entries<EntityRootBase>()
+                .Where(x =>
+                    x.Entity.DomainEvents != null &&
+                    x.Entity.DomainEvents.Any())
+                .ToImmutableList();
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.DomainEvents)
+                .ToImmutableList();
+
+            domainEntities.ForEach(entity => entity.Entity.DomainEvents.Clear());
+
+            return domainEvents;
+        }
+    }
+}
