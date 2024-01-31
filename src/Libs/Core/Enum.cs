946c351d0f81e7173b57da1016baff6ab6f115fd
hungtq1016 <hungtq1016@gmail.com>
2024-01-31 18:02:34 +0700

    update: fix error

---
+++ src/Libs/Core/Enum.cs
@@ -0,0 +1,14 @@
+﻿namespace Core
+{
+    public enum StatusMessageEnum
+    {
+        Successfully = 200,
+        Created = 201,
+        BadRequest = 400,
+        UnauthorizedAccess = 401,
+        Forbidden = 403,
+        NotFound = 404,
+        NotSupported = 415,
+        UnexpectedError = 500
+    }
+}
