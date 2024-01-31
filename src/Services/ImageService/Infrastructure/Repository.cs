d9acb0a324fb49404517aad4c83a570762811e99
hungtq1016 <hungtq1016@gmail.com>
2024-01-31 18:04:23 +0700

    enhancement: add image service

---
+++ src/Services/ImageService/Infrastructure/Repository.cs
@@ -0,0 +1,15 @@
+﻿using Core;
+using ImageService.Infrastructure.Data;
+using ImageService.Models;
+using Infrastructure.EFCore.Repository;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ImageService.Infrastructure
+{
+    public class ImageRepository : RepositoryBase<ImageContext, Image>  
+    {
+        public ImageRepository(ImageContext context, IMemoryCache cache) : base(context, cache)
+        {
+        }
+    }
+}
